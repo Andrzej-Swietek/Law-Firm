be99a0938a5b7a5100eef1acbbdb340753f8a856
Andrzej Świętek <mr.andi321@gmail.com>
2025-01-15 01:33:31 +0100

    Added docs and adding env support

--- law-firm/src/main/kotlin/pl/swietek/law_firm/LawFirmApplication.kt
+++ law-firm/src/main/kotlin/pl/swietek/law_firm/LawFirmApplication.kt
@@ -1,5 +1,6 @@
 package pl.swietek.law_firm
 
+import io.github.cdimascio.dotenv.dotenv
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
@@ -7,5 +8,9 @@ import org.springframework.boot.runApplication
 class LawFirmApplication
 
 fun main(args: Array<String>) {
+	val dotenv = dotenv()
+	System.setProperty("DB_USERNAME", dotenv["DB_USERNAME"] ?: "")
+	System.setProperty("DB_PASSWORD", dotenv["DB_PASSWORD"] ?: "")
+
 	runApplication<LawFirmApplication>(*args)
 }
